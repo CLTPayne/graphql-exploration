7a524ed0d7e9ed5fb8edbcd43fa6cd60594ba15d
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-12-22 00:18:33 +0000

    Add getWelcome schema

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    getWelcome: String
+  }
+`);
